10ed337ad1e8405bb8e4f43b50f59faddc2dc250
HeoJeongJin <serpiko@urbandatalab.co.kr>
2022-03-11 18:06:53 +0900

    HttpExceptionFilter로 throw new 공통처리

---
+++ src/exception/HttpExceptionFilter.ts
@@ -0,0 +1,27 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+// src/exception/except.controller.ts 에서는 HttpException을 토대로 throw 한 에러를 직접 설정하였다
+// ExceptionFilter 는 데코레이터를 해당 ExceptionFilter의 객체에 주입하여 response 체계를 공통으로 처리할 수 있는 코드를 작성해 본다.
+// 콘트롤러에서 HttpExceptionFilter 를 사용하고자 하는 컨트롤러의 Route 에서 UseFilters 라는 데코레이터를 이용하여 사용할 수 있다.
+
+// catch 데코레이터를 사용하여 HttpException 관련 에러를 catch 하겠다는 선언
+@Catch(HttpException)
+
+// ExceptionFilter 인터페이스 implements
+export class HttpExceptionFilter implements ExceptionFilter {
+  // catch 함수를 구현
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    // response 객체를 통해 statusCode, timestamp, 에러가 발생한 path를 출력
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
--- src/exception/except.controller.ts
+++ src/exception/except.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param, UseFilters } from '@nestjs/common';
+import { HttpExceptionFilter } from './HttpExceptionFilter';
 
+// 내장된 Http Exception 에러 사용
 @Controller('except')
 export class ExceptController {
-  @Get(':code')
-  executeError(@Param('code') code: HttpStatus) {
+  @Get('/manual/:code')
+  executeErrorManual(@Param('code') code: HttpStatus) {
+    if (code >= 200 && code < 400) {
+      return 'status ok';
+    } else if (code == 403) {
+      // 메시지는 아래와 같이 커스텀하게 담을 수 있다
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'Forbidden Error',
+        },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
+
+  // throw 된 HttpException 에 관련된 모든 에러들은 HttpExceptionFilter 에서 처리하게 된다
+  // 실제 각 Exception 별로 에러를 공통화 시킬 수 있다.
+  @Get('/common/:code')
+  @UseFilters(new HttpExceptionFilter())
+  executeErrorCommon(@Param('code') code: HttpStatus) {
     if (code >= 200 && code < 400) {
       return 'status ok';
     } else if (code == 403) {
